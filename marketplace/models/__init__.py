a3dee23496914d5b0e7927432a5e1abeea3edab7
Philip Odame <odamephilip966@gmail.com>
2026-02-15 01:35:15 +0000

    fix: refactor marketplace models into package to resolve deployment import error

---
+++ marketplace/models/__init__.py
@@ -0,0 +1,15 @@
+from .faq import FAQ
+from .feedback import Feedback
+from .failed_task import FailedTask
+from .notification import Notification
+from .legal import LegalDocument
+from .special_offer import SpecialOffer
+
+__all__ = [
+    'FAQ',
+    'Feedback',
+    'FailedTask',
+    'Notification',
+    'LegalDocument',
+    'SpecialOffer',
+]
---
+++ marketplace/models/faq.py
@@ -0,0 +1,21 @@
+import uuid
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+class FAQ(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    question = models.CharField(max_length=255)
+    answer = models.TextField()
+    is_active = models.BooleanField(default=True)
+    order = models.PositiveIntegerField(default=0)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _('FAQ')
+        verbose_name_plural = _('FAQs')
+        ordering = ['order', '-created_at']
+
+    def __str__(self):
+        return self.question
