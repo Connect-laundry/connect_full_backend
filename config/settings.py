d627405d571bde663530a44d705b16d07dc02b60
PhilipTheBackendDeveloper <odamephilip966@gmail.com>
2026-03-27 18:14:31 +0000

    refresh token from 30 days to 90 days

--- config/settings.py
+++ config/settings.py
@@ -313,7 +313,7 @@ CACHES = {
 from datetime import timedelta
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=100),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': True,
